f52006497873bceba591a47ccb24e79874709d5f
Alex Harvey <alex@pixelfactor.ca>
2025-12-12 00:01:20 -0800

    Fix vfo write issue

--- src/components/channels/ChannelsTable.tsx
+++ src/components/channels/ChannelsTable.tsx
@@ -554,17 +554,7 @@ export const ChannelsTable: React.FC<ChannelsTableProps> = ({ channels: channels
           onClose={() => setEditingChannel(null)}
           channel={editingChannel}
           onSave={(updatedChannel) => {
-            // Handle VFO channels (4001 and 4002) - update via radioSettings
-            if (isVFOChannel(updatedChannel.number)) {
-              if (updatedChannel.number === 4001 && radioSettings?.vfoA) {
-                updateSettings({ vfoA: updatedChannel });
-              } else if (updatedChannel.number === 4002 && radioSettings?.vfoB) {
-                updateSettings({ vfoB: updatedChannel });
-              }
-            } else {
-              // Regular channels - update via channels store
-              updateChannel(updatedChannel.number, updatedChannel);
-            }
+            updateChannel(updatedChannel.number, updatedChannel);
             setEditingChannel(null);
           }}
         />
